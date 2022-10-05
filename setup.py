3bd84a19916d07cf0da7f5ef955af7375236ac79
Petr Jindra <petr@jindrovi.uno>
2022-10-06 00:18:50 +0200

    project init

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from __future__ import annotations
+
+from os import path
+
+from setuptools import find_packages, setup
+
+
+SRC = path.join(path.dirname(__file__), "src")
+REQUIRE_FILE = path.join(path.dirname(__file__), "requirements.txt")
+
+
+def get_requirements():
+    with open(REQUIRE_FILE, "r") as fd:
+        return [r for r in fd]
+
+
+def main():
+    setup(
+        name="cadquery-common-part-library",
+        fullname="CadQuery 2 Common Part Library",
+        package_dir={"": SRC},
+        packages=find_packages(SRC),
+        install_requires=get_requirements(),
+        data_files=[("", ["requirements.txt"])]
+    )
+
+
+if __name__ == "__main__":
+    main()
